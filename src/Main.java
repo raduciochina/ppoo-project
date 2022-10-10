fc26991f137a43cdb40b574372aec07c384141de
Ciochina <rciochin@techteam.com>
2022-10-10 09:12:28 +0300

    trying a commit on different machine

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import models.Restaurant;
+
 public class Main {
     public static void main(String[] args) {
-
+        Restaurant r =Restaurant.getInstance("La Radu", "Bd. Compozitorilor 33");
     }
 }
